179f8f2cc92aaacb6d3df8e1b425dfd2b08f349f
Jennifer Schultz <jennifer.schultz@duke.edu>
2021-01-23 11:56:55 -0500

    Added API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
 function Header() {
+
   return (
     <div>
       <nav>
-        <h1>Hello World!</h1>
-          <ul>
-            <li>Thing 1</li>
-            <li>Thing 2</li>
-            <li>Thing 3</li>
-          </ul>
+        <h1>DAML Weather Forecast</h1>
         </nav>
         <main>
-          <p>This is where most of my content will go.</p>
+          <p></p>
         </main>
     </div>
   );
